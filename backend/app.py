6c48a9ab80896df92ac2083c10776d7b3c8e3c19
Dyotak <dyotak10@gmail.com>
2024-04-03 01:32:16 +0530

    deployment commit

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import logging
 
 app = Flask(__name__)
 CORS(app)
@@ -11,10 +13,10 @@ def hello_world():
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-    # You can now use the data from the request body
-    # For example, print it to the console
     prediction = True
     return jsonify({"prediction":prediction})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    logging.basicConfig(filename='app.log', level=logging.INFO)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
